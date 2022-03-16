4d6b2da63024b0000b4bd58e53373eac367adbd5
James Smith <theworld969@hotmail.com>
2022-03-15 20:06:12 -0700

    Action Cable complete

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,11 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  
+  # Below line equivalent to the above
+  broadcasts_to -> (quote) { "quotes" }, inserts_by: :prepend
 end
