866788e715f850c82a23068ffa314601b7d8c21d
James Smith <theworld969@hotmail.com>
2022-03-18 11:41:10 -0600

    Disable turbo for flash message testing

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// Disables turbo
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
