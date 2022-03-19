676be7c5500ec83681a8091c05764644e2d17633
James Smith <theworld969@hotmail.com>
2022-03-19 10:38:49 -0600

    Add LineItemDates model and add relations

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,12 +3,8 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  # after_update_commit -> { broadcast_replace_later_to "quotes" }
-  # after_destroy_commit -> { broadcast_remove_to "quotes" }
-  
-  # Below line equivalent to the above
   broadcasts_to -> (quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 
   belongs_to :company
+  has_many :line_item_dates, dependent: :destroy
 end
