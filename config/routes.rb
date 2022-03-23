9b94a9bb41710785c52568b88899f25dc123de78
James Smith <theworld969@hotmail.com>
2022-03-23 11:08:47 -0600

    CRUD line_items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :quotes do
-    resources :line_item_dates, except: [:show, :index]
+    resources :line_item_dates, except: [:show, :index] do
+      resources :line_items, except: [:show, :index]
+    end
   end
 
   root "pages#home"
